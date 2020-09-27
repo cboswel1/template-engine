e376ca3ed633cc32c39c8eeb41138f9e7b6bce05
cboswel1 <christianboswell86@gmail.com>
2020-09-27 10:41:07 -0600

    employee.js class build

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,31 @@
 // TODO: Write code to define and export the Employee class
+
+
+//Can instantiate Employee instance
+
+class Employee {
+    constructor() {
+    //"Can set name via constructor arguments"
+    this.name = name; 
+    //"Can set id via constructor argument" 
+    this.id = id;
+    //"Can set email via constructor argument"
+    this.email = email; 
+    }
+    //Can get name via getName()
+    getName() {
+
+    }
+    //Can get id via getId()
+    getId() {
+
+    }
+    //Can get email via getEmail()
+    getEmail() {
+
+    }
+    //getRole() should return \"Employee\
+    getRole() {
+        
+    }
+}
